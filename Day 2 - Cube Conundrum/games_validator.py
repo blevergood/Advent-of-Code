e95a8ef50cbf03d325476f7f4f144aa45545984c
Brad Levergood <brad.levergood@gmail.com>
2023-12-04 15:23:38 -0500

    Part 1 solution

---
+++ Day 2 - Cube Conundrum/games_validator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+def validate_games(input):
+    f = open(input, 'r')
+    games = [validate_game(game) for game in f.readlines()]
+    f.close()
+    return sum(games)
+
+def validate_game(game):
+    game_t, rounds_t = game.split(": ")
+    game_n = int(game_t.split(" ")[1])
+    rounds = [get_round_map(round) for round in rounds_t.split("; ")]
+    rounds_valid = [validate_round(round) for round in rounds]
+    if False in rounds_valid:
+        return 0
+    return game_n
+
+def get_round_map(round):
+    balls = round.split(', ')
+    round_map = {}
+    for ball in balls:
+        v, k = ball.split(" ")
+        round_map[k.strip()] = int(v)
+    return round_map
+
+def validate_round(round, limits={"red": 12, "green": 13, "blue": 14}):
+    for key in round.keys():
+        if round[key] > limits[key]:
+            return False
+    return True
+
+if __name__ == "__main__":
+    print(validate_games("./puzzle input.txt"))
