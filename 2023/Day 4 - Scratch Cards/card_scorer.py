0c8062970b46d74f52d955aa52db1c248af224f4
Brad Levergood <brad.levergood@gmail.com>
2023-12-07 17:22:17 -0500

    Day 4 part 2

--- 2023/Day 4 - Scratch Cards/card_scorer.py
+++ 2023/Day 4 - Scratch Cards/card_scorer.py
@@ -16,6 +16,7 @@ class Card:
             self.points = 2 ** (self.score - 1)
         else:
             self.points = self.score
+        self.copies = 1
 
 
 def get_card_points(source: str) -> int:
@@ -29,5 +30,23 @@ def get_card_points(source: str) -> int:
     return sum(scores)
 
 
+def get_card_copies(source: str) -> int:
+    f = open(source, "r")
+    lines = f.read().split("\n")
+
+    cards = [Card(line) for line in lines]
+
+    for i in range(len(cards)):
+        if cards[i].score > 0:
+            j = 1
+            while j <= cards[i].score:
+                cards[i + j].copies += cards[i].copies
+                j += 1
+
+    copies = [card.copies for card in cards]
+    return sum(copies)
+
+
 if __name__ == "__main__":
     print("Part 1:", get_card_points("./puzzle input.txt"))
+    print("Part 2:", get_card_copies("./puzzle input.txt"))
