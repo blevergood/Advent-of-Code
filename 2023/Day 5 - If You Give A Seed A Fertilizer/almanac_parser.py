dc3b81dcd829490cc6567a29dada8095813884a6
Brad Levergood <brad.levergood@gmail.com>
2023-12-10 00:46:18 -0500

    Day 5 part 2

--- 2023/Day 5 - If You Give A Seed A Fertilizer/almanac_parser.py
+++ 2023/Day 5 - If You Give A Seed A Fertilizer/almanac_parser.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import re
+import sys
+from collections.abc import Callable
+from bidict import bidict
 
 
 class AlmanacRange:
     def __init__(self, start: int, length: int) -> None:
         self.start = start
-        self.end = start + length
+        self.end = start + length - 1
 
     def contains(self, value: int) -> bool:
         if value < self.start:
@@ -15,15 +18,24 @@ class AlmanacRange:
         else:
             return True
 
+    def get_bounds(self) -> list[int]:
+        return [self.start - 1, self.start, self.end, self.end + 1]
+
+    def expand(self) -> list[int]:
+        return list(range(self.start, self.end + 1))
+
 
 class AlmanacMap:
     def __init__(self, ranges: list[list[int]]) -> None:
-        self.mapping = dict()
+        self.mapping = bidict()
         for r in ranges:
             dest_start, src_start, length = r
             self.mapping[AlmanacRange(src_start, length)] = AlmanacRange(
                 dest_start, length
             )
+
+    def get_inverse(self):
+        return self.mapping
 
     def mapped_number(self, source: int) -> int:
         for k in self.mapping.keys():
@@ -31,12 +43,83 @@ class AlmanacMap:
                 return self.mapping[k].start + (source - k.start)
         return source
 
+    def inverted_number(self, dest: int) -> int:
+        for k in self.mapping.inverse.keys():
+            if k.contains(dest):
+                return self.mapping.inverse[k].start + (dest - k.start)
+        return dest
 
-def get_closest_location(source: str) -> int:
+
+def get_maps(segments: list[str]) -> list[AlmanacMap]:
+    return [
+        AlmanacMap(
+            [
+                [int(match) for match in re.findall(r"\d+", map)]
+                for map in segment.split("\n")[1:]
+            ]
+        )
+        for segment in segments
+    ]
+
+
+# Part 1
+def get_seeds_list(line: str) -> list[int]:
+    return [int(match) for match in re.findall(r"\d+", line)]
+
+
+# Part 2
+def get_seeds_ranges(line: str) -> list[AlmanacRange]:
+    numbers = get_seeds_list(line)
+    return [AlmanacRange(*numbers[i : i + 2]) for i in range(0, len(numbers), 2)]
+
+
+def get_seeds_to_check(line: str, maps=list[AlmanacMap]) -> list[int]:
+    """
+    The challenge here is that it takes too long to manually process all available seeds
+    We only need to check the bounds of seeds corresponding to each map's piecewise equations
+    Concept from:
+    https://www.reddit.com/r/adventofcode/comments/18b4b0r/comment/kc3q9c6/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    """
+    seed_ranges = get_seeds_ranges(line)
+
+    # Bounds of locations is entire range
+    potential_seeds = [0, sys.maxsize]
+
+    # Figure out bounds by walking backwards through mapping
+    for map in reversed(maps):
+        # Only iterate over seeds of the previous layers (don't double-translate new values)
+        current_seeds = len(potential_seeds)
+        # Translation of each previous layer boundary to current-layer values
+        for i in range(current_seeds):
+            potential_seeds.append(map.inverted_number(potential_seeds[i]))
+        # Add current-layer boundaries
+        for r in map.mapping.keys():
+            potential_seeds.extend(r.get_bounds())
+        # Eliminate any duplicates
+        potential_seeds = list(set(potential_seeds))
+
+    seeds_to_check = []
+
+    # Intersection between bounds-based potential seeds and available seeds in the ranges
+    for r in seed_ranges:
+        for seed in potential_seeds:
+            if r.contains(seed):
+                seeds_to_check.append(seed)
+
+    return seeds_to_check
+
+
+def get_closest_location(source: str, func: Callable[[str], list[int]]) -> int:
     f = open(source, "r")
     segments = f.read().split("\n\n")
 
-    seeds = [int(match) for match in re.findall(r"\d+", segments.pop(0))]
+    arguments = [segments.pop(0)]
+    maps = get_maps(segments)
+
+    if func == get_seeds_to_check:
+        arguments.append(maps)
+
+    seeds = func(*arguments)
 
     (
         seed_soil,
@@ -46,15 +129,7 @@ def get_closest_location(source: str) -> int:
         light_temp,
         temp_humid,
         humid_location,
-    ) = [
-        AlmanacMap(
-            [
-                [int(match) for match in re.findall(r"\d+", map)]
-                for map in segment.split("\n")[1:]
-            ]
-        )
-        for segment in segments
-    ]
+    ) = maps
 
     locations = [
         humid_location.mapped_number(
@@ -76,4 +151,5 @@ def get_closest_location(source: str) -> int:
 
 
 if __name__ == "__main__":
-    print("Part 1:", get_closest_location("./puzzle input.txt"))
+    print("Part 1:", get_closest_location("./puzzle input.txt", get_seeds_list))
+    print("Part 2:", get_closest_location("./puzzle input.txt", get_seeds_to_check))
