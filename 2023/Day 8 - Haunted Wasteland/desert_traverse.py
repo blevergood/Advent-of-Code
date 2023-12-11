7e28ea20cf6582126d39c9d7ffe01149f4b08c29
Brad Levergood <brad.levergood@gmail.com>
2023-12-11 16:29:07 -0500

    Day 8 Part 2

--- 2023/Day 8 - Haunted Wasteland/desert_traverse.py
+++ 2023/Day 8 - Haunted Wasteland/desert_traverse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import re
+from math import lcm
 
 
 def get_graph(nodes: str) -> dict[str, dict[str, str]]:
@@ -9,19 +10,53 @@ def get_graph(nodes: str) -> dict[str, dict[str, str]]:
     }
 
 
-def find_req_steps(input: str) -> int:
-    f = open(input, "r")
-    directions, nodes = f.read().split("\n\n")
-    graph = get_graph(nodes)
+def graph_traversal(
+    graph: dict[str, dict[str, str]], start, end, directions: str
+) -> int:
     steps = 0
     i = 0
-    current_node = "AAA"
-    while current_node != "ZZZ":
+    current_node = start
+    # Part 1
+    if end:
+        condition = lambda x: x != "ZZZ"
+    # Part 2
+    else:
+        condition = lambda x: "Z" not in x
+    while condition(current_node):
         direction = directions[i]
         current_node = graph[current_node][direction]
         steps += 1
         i = (i + 1) % len(directions)
+
     return steps
+
+
+# Part 1
+def find_req_steps(input: str) -> int:
+    f = open(input, "r")
+    directions, nodes = f.read().split("\n\n")
+    f.close()
+    graph = get_graph(nodes)
+    return graph_traversal(graph, "AAA", "ZZZ", directions)
+
+
+# Part 2
+def find_steps_all_exits(input: str) -> int:
+    f = open(input, "r")
+    directions, nodes = f.read().split("\n\n")
+    f.close()
+    graph = get_graph(nodes)
+    starting_nodes = [node for node in graph.keys() if "A" in node]
+    # Find all successful paths independently
+    all_paths = [
+        graph_traversal(graph, node, "", directions) for node in starting_nodes
+    ]
+
+    # Providing the least-common multiple of all paths will give you the first
+    # all paths will match up, which is the answer
+    return lcm(*all_paths)
+
 
 if __name__ == "__main__":
     print(f"Part 1: {find_req_steps('puzzle input.txt')}")
+    print(f"Part 2: {find_steps_all_exits('puzzle input.txt')}")
