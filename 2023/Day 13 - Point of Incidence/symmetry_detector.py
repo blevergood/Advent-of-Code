1599d2128c95e45325c49cc8d970f79b04376c8e
Brad Levergood <brad.levergood@gmail.com>
2023-12-16 22:33:48 -0500

    Day 13 Part 2

--- 2023/Day 13 - Point of Incidence/symmetry_detector.py
+++ 2023/Day 13 - Point of Incidence/symmetry_detector.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+
+# Solution from: https://advent-of-code.xavd.id/writeups/2023/day/13/
+# Had trouble conceptualizing symmetry in a list of strings.
+
+
+# Used different variable names and added to help myself cement understanding of why this works.
 def handle_input(input: str) -> list[list[str]]:
     f = open(input, "r")
     segments = [segment.split("\n") for segment in f.read().split("\n\n")]
@@ -6,24 +12,43 @@ def handle_input(input: str) -> list[list[str]]:
     return segments
 
 
-def reflection(segment: list[str]) -> int:
+# You need to find the substring with exactly one difference
+# This function will return 0 for symmetrical strings and large numbers
+# For non-symmetrical strings, but exactly 1 for the string with the "smudge"
+def str_differences(left: str, right: str) -> int:
+    # Booleans are counted as 0/1 and can be summed together
+    return sum(a != b for a, b in zip(left, right))
+
+
+# For each index, check substring equality along that "line"
+# Plug in the original list as well as a rotated copy of the list
+# to check both dimensions of symmetry
+def reflection(segment: list[str], num_smudges: int) -> int:
     # 0 can't be the reflection index
     for index in range(1, len(segment)):
-        if all(
-            left == right
-            for left, right in zip(reversed(segment[:index]), segment[index:])
+        if (
+            sum(
+                str_differences(left, right)
+                for left, right in zip(reversed(segment[:index]), segment[index:])
+            )
+            == num_smudges
         ):
             return index
     return 0
 
 
-def get_score(segment: list[str]) -> int:
-    if row := reflection(segment):
+def get_score(segment: list[str], num_smudges: int) -> int:
+    if row := reflection(segment, num_smudges):
         return 100 * row
-    if col := reflection(list(zip(*segment))):
+    # list(zip()) rotates the contents if each string is the same length (which is guaranteed in this case).
+    # Also see og solution for 2023/Day 11 - Cosmic Expanse
+    if col := reflection(list(zip(*segment)), num_smudges):
         return col
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     segments = handle_input("puzzle input.txt")
-    scores = [get_score(segment) for segment in segments]
+    scores = [get_score(segment, 0) for segment in segments]
     print(f"Part 1: {sum(scores)}")
+    smudge_scores = [get_score(segment, 1) for segment in segments]
+    print(f"Part 2: {sum(smudge_scores)}")
