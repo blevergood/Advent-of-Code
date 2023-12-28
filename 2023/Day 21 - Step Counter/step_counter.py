2f61eaf01f5b4c03b054ec923c64ad82e5d863a1
Brad Levergood <brad.levergood@gmail.com>
2023-12-27 19:06:22 -0500

    Day 21 Optimize Part 1. Do Part 2

--- 2023/Day 21 - Step Counter/step_counter.py
+++ 2023/Day 21 - Step Counter/step_counter.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+from collections import defaultdict
+from collections.abc import Callable
+from collections.abc import Generator
+
+
 def handle_input(input: str) -> list[str]:
     f = open(input, "r")
     grid = f.read().split("\n")
@@ -13,32 +18,79 @@ def get_start(grid: list[str]) -> (int, int):
             return (x, y)
 
 
+# https://github.com/CalSimmon/advent-of-code/blob/main/2023/day_21/solution.py
+# Part 1
+# BFS to get number of tiles for any given distance from the starting point
+def possible_titles(
+    tile: tuple[int, int], grid: list[str]
+) -> Generator[tuple[int, int]]:
+    directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    for direction in directions:
+        new_point = (tile[0] + direction[0], tile[1] + direction[1])
+        if grid[new_point[1] % len(grid)][new_point[0] % len(grid)] != "#":
+            yield new_point
+
+
 def get_possible_tiles(
     grid: list[str], start: tuple[int, int], steps: int
-) -> list[tuple[int, int]]:
-    to_visit: list[tuple[int, int]] = [start]
-    steps_taken = 0
-    while steps_taken < steps:
-        steps_taken += 1
-        i = len(to_visit)
-        for j in range(i):
-            current_tile = to_visit.pop(0)
-            curr_x, curr_y = current_tile
-            for tile in [
-                (curr_x - 1, curr_y),
-                (curr_x + 1, curr_y),
-                (curr_x, curr_y - 1),
-                (curr_x, curr_y + 1),
-            ]:
-                x, y = tile
-                if y in range(len(grid)):
-                    if x in range(len(grid[y])):
-                        if grid[y][x] != "#" and tile not in to_visit:
-                            to_visit.append(tile)
-    print(to_visit)
-    return len(to_visit)
+) -> dict[int, int]:
+    tiles = defaultdict(int)
+    visited = set()
+    to_visit = [(start, 0)]
+    while to_visit:
+        current_tile, distance = to_visit.pop(0)
+        if distance == steps + 1 or current_tile in visited:
+            continue
+        tiles[distance] += 1
+        visited.add(current_tile)
+
+        for next_tile in possible_titles(current_tile, grid):
+            to_visit.append((next_tile, distance + 1))
+    return tiles
+
+
+def calculate_possible_tiles(
+    grid: list[str], start: tuple[int, int], steps: int
+) -> int:
+    distance_to_tiles = get_possible_tiles(grid, start, steps)
+    return sum(
+        num_tiles
+        for distance, num_tiles in distance_to_tiles.items()
+        # Because you can step back and forth onto tiles in subsequent steps, only
+        # sum every other tile based on the number of steps
+        # (i.e. only the odds for an odd number of steps etc.)
+        if (distance % 2 == steps % 2)
+    )
+
+
+# Part 2
+# For this one, you can't brute force it.
+# Number of possible tiles is a quadratic
+# Can use 1st 3 points to determine an equation
+# In this case, the fit for small numbers is slightly off for big numbers
+# and vice versa.
+# So use datapoints f(S), f(S + X), f(S + 2*X)
+#   - S = starting point = len(grid)/2
+#   - X = grid size = len(grid)
+def get_quadratic(points: list[int]) -> Callable:
+    a = (points[2] - (2 * points[1]) + points[0]) // 2
+    b = points[1] - points[0] - a
+    c = points[0]
+
+    return lambda x: (a * x**2) + (b * x) + c
+
+
+def get_infinite_grid_tiles(grid, start, goal):
+    X = len(grid)
+    S = X // 2
+
+    points = [calculate_possible_tiles(grid, start, S + (i * X)) for i in range(3)]
+    quad = get_quadratic(points)
+    return quad(((goal - S) // X))
+
 
 if __name__ == "__main__":
     grid = handle_input("puzzle input.txt")
     start = get_start(grid)
-    print(f"Part 1: {get_possible_tiles(grid, start, 64)}")
+    print(f"Part 1: {calculate_possible_tiles(grid, start, 64)}")
+    print(f"Part 2: {get_infinite_grid_tiles(grid, start, 26501365)}")
