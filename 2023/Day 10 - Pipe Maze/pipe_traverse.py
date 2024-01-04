f9fff38363727ab45eebf1f057de868358a2ea41
Brad Levergood <brad.levergood@gmail.com>
2024-01-04 16:33:11 -0500

    Made day 10 waaaay faster

--- 2023/Day 10 - Pipe Maze/pipe_traverse.py
+++ 2023/Day 10 - Pipe Maze/pipe_traverse.py
@@ -10,23 +10,26 @@ class Pipe:
         self.value = value
         self.x = x
         self.y = y
-        shapes = {
-            "|": ["above", "below"],
-            "-": ["left", "right"],
-            "L": ["above", "right"],
-            "J": ["above", "left"],
-            "7": ["left", "below"],
-            "F": ["right", "below"],
-            "S": ["above", "below", "right", "left"],
-            ".": [],
+        shapes_to_dirs = {
+            "|": {(0, -1), (0, 1)},
+            "-": {(-1, 0), (1, 0)},
+            "L": {(1, 0), (0, -1)},
+            "J": {(-1, 0), (0, -1)},
+            "7": {(-1, 0), (0, 1)},
+            "F": {(1, 0), (0, 1)},
+            "S": {(0, 1), (1, 0), (0, -1), (-1, 0)},
+            ".": set(),
         }
-        self.shape = shapes[value]
+        self.dirs = shapes_to_dirs[value]
 
     def __hash__(self) -> int:
         return hash("{self.value}{self.x}{self.y}")
 
     def __str__(self) -> str:
         return f"{(self.value, self.x, self.y)}"
+
+    def __repr__(self) -> str:
+        return str(self)
 
     def __eq__(self, other: "Pipe") -> bool:
         if isinstance(other, Pipe):
@@ -39,26 +42,26 @@ class Pipe:
                 (
                     other.x == self.x - 1
                     and other.y == self.y
-                    and "left" in self.shape
-                    and "right" in other.shape
+                    and (-1, 0) in self.dirs
+                    and (1, 0) in other.dirs
                 )
                 or (
                     other.x == self.x + 1
                     and other.y == self.y
-                    and "right" in self.shape
-                    and "left" in other.shape
+                    and (1, 0) in self.dirs
+                    and (-1, 0) in other.dirs
                 )
                 or (
                     other.y == self.y - 1
                     and other.x == self.x
-                    and "above" in self.shape
-                    and "below" in other.shape
+                    and (0, -1) in self.dirs
+                    and (0, 1) in other.dirs
                 )
                 or (
                     other.y == self.y + 1
                     and other.x == self.x
-                    and "below" in self.shape
-                    and "above" in other.shape
+                    and (0, 1) in self.dirs
+                    and (0, -1) in other.dirs
                 )
             )
         return NotImplemented
@@ -69,54 +72,46 @@ class Maze:
     def __init__(
         self, dimensions: dict[str, int], pipes: dict[int, list[Pipe]]
     ) -> None:
-        self.pipes = dict()
+        self.pipes: list[pipes] = []
         self.dimensions = dimensions
-        self.start = None
-        queue = []
-        visited = []
+        self.start: Pipe = None
         for key in pipes.keys():
             self.start = next((x for x in pipes[key] if x.value == "S"), None)
             if self.start is not None:
                 break
 
-        # Function to populate the map with nodes and adjacency lists
-        # Part 1 - modified
-        queue.append(self.start)
-        visited.append(self.start)
-        while queue:
-            a = queue.pop(0)
-            for x, y in [
-                (a.x + 1, a.y),
-                (a.x - 1, a.y),
-                (a.x, a.y + 1),
-                (a.x, a.y - 1),
-            ]:
-                if y in pipes.keys():
-                    if x < len(pipes[y]):
-                        neighbor = pipes[y][x]
-                        if a.is_acceptable_match(neighbor):
-                            if neighbor not in visited:
-                                visited.append(neighbor)
-                                queue.append(neighbor)
-                            if a in self.pipes:
-                                self.pipes[a].append(neighbor)
-                            else:
-                                self.pipes[a] = [neighbor]
+        """
+        Function to populate the map with nodes and adjacency lists
+        Part 1
+        
+        We know from the examples and the puzzle input that the
+        Starting node `S` will only have two characters that it's compatible
+        i.e. it's either on an edge or surrounded on left/right by `|` characters
+        etc.
 
-    # Part 2
-    # Points need to be in (counter-)clockwise order for shoelace formula to work
-    def get_loop_traversal_order(self) -> list[Pipe]:
-        ordered_points = [self.start]
-        visited = [self.start]
-        begin = self.pipes[self.start][0]
-        while begin != self.start:
-            ordered_points.append(begin)
-            visited.append(begin)
-            if self.pipes[begin][0] not in visited:
-                begin = self.pipes[begin][0]
-            else:
-                begin = self.pipes[begin][1]
-        return ordered_points
+        So we just have to find the first compatible node and then move forward.
+        Every other character can only move two ways, so if we know which of those ways
+        we're coming from, it just has to pull the node in from the other direction
+        until it hits the starting node again.
+
+        Using a list so that the points are already ordered for Part 2
+        """
+        self.pipes.append(self.start)
+        for next_x, next_y in self.start.dirs:
+            x, y = self.start.x + next_x, self.start.y + next_y
+            if y in pipes.keys():
+                if x < len(pipes[y]):
+                    next_tile = pipes[y][x]
+                    if self.start.is_acceptable_match(next_tile):
+                        current_tile = next_tile
+                        coming_from = {(-next_x, -next_y)}
+                        break
+        while current_tile != self.start:
+            self.pipes.append(current_tile)
+            next_x, next_y = list(current_tile.dirs - coming_from)[0]
+            x, y = current_tile.x + next_x, current_tile.y + next_y
+            coming_from = {(-next_x, -next_y)}
+            current_tile = pipes[y][x]
 
 
 # Part 1
@@ -173,27 +168,13 @@ def get_interior_points(area: int, boundary_points: int) -> int:
 
 
 if __name__ == "__main__":
-    maze = get_maze("./puzzle input.txt")
+    maze = get_maze("puzzle input.txt")
     pipes = maze.pipes
-    # TODO: Make this more efficient
-    # TODO:     Specifically, other than S, we know that a character should only have 2 directions to check
-    # TODO:     based on the character
-    # TODO:     Further, since we know the previous character, we can just grab the next one
-    # TODO:     That will allow us to do the straight traversal around the loop
-    # TODO:     And we can remove the traversal order step from p2
 
-    # The above works because our input happens to only have 2 proper connections
-    # Even though we start on an S
-
-    # To more generalize this, we would set up a back-tracking condition so if we
-    # go back to start if we can't move forward on a given tile
-
-    # Using TODO for all of the above lines because my vscode highlights it.
-    
     # since connections can only make right-angle turns, there have to be an even number of pipes
     # So the furthest pipe will be the half-way point of the loop.
     print(f"Part 1: {len(pipes)/2}")
 
     print(
-        f"Part 2: {get_interior_points(get_area(maze.get_loop_traversal_order()), len(pipes))}"
+        f"Part 2: {get_interior_points(get_area(pipes), len(pipes))}"
     )
